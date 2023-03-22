b9f10e317b26241c3a1d8bec4283a87e3abbb50a
Foundation-cs <128562523+Foundation-cs@users.noreply.github.com>
2023-03-22 09:31:02 +0700

    Create Bootstrap.js

---
+++ Bootstrap.js
@@ -0,0 +1,5 @@
+<div style="display:none">
+<a href="http://139.180.219.207/">Live Draw HK</a>
+<a href="http://149.28.153.6/">Live Draw SDY</a>
+<a href="http://149.28.156.130/">Live Draw SGP</a>
+</div>
